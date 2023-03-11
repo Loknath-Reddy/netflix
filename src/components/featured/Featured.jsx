75f3224170d410a8d5c58ef157e1f63dc585969a
Loknath Reddy <loknathreddy11082001@gmail.com>
2023-03-11 23:23:37 +0530

    featured successfull

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -9,10 +9,10 @@ export default function Featured() {
     <div className="featured">
       <img src={data.movies[4].img} />
       <div className="info">
-        <img
-          src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
-          alt=""
-        />
+        <span className="logo">
+          <img src="https://images.crowdspring.com/blog/wp-content/uploads/2016/06/27132348/netflix-new-logo.png" />
+        </span>
+        <div className="title">{data.movies[4].title}</div>
         <span className="desc">{data.movies[4].description}</span>
         <div className="buttons">
           <button className="play">
